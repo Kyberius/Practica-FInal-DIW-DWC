3ba94ee2390a84f3024c7416d0275fe661600f69
Alex Valencia <Alex Valencia>
2016-01-30 14:23:12 +0100

    Model Updated

--- js/Model.js
+++ js/Model.js
@@ -4,12 +4,7 @@ var ClientesCollection = (function() {
 	var clientes;
 	var ReadAll = function() {
 		$.post("CRUD.php",function(response) {
-			clientes = response;
-		},"JSON");
-	}
-	var ReadOne = function() {
-		$.post("CRUD.php",function(response) {
-
+			clientes = $.makeArray(response);
 		},"JSON");
 	}
 
@@ -21,9 +16,22 @@ var ClientesCollection = (function() {
 	var GetById = function(id) {
 
 	}
-	
 
+	var delete = function(arrayId) {
+		delete clientes[arrayId];
+	}
 
+	var sortById = function() {
+		clientes.sort(funtion(a,b) {
+			return a.getId() - b.getId();
+		})
+	}
+
+	var sortByName = function() {
+		clientes.sort(function(a,b) {
+			return a.getNombre()<b.getNombre()?-1:a.getNombre()>b.getNombre()?1:0;
+		})
+	}
 	//Reveal
 	var my = {
 		init:init,
@@ -36,10 +44,24 @@ var ClientesCollection = (function() {
 var Cliente = (function() {
 	//Private
 	var id,nombre,ciudad,sexo,telefono,fechaNacimiento;
-	//TODOS los metodos de gestion del modulo. 
+	//TODOS los metodos de gestion del modulo.
+	var fill = function (id,nombre,ciudad,sexo,telefono,fechaNacimiento) {
+		id = id;
+		nombre = nombre;
+		ciudad = ciudad;
+		sexo = sexo;
+		telefono = telefono;
+		fechaNacimiento = fechaNacimiento;
+	}
+
+	var empty = function () {
+		id = null;
+		nombre = null;
+	}
 	//Public
 	
 	//Reveal
-	my = {
+	return {
+
 	}
 });
