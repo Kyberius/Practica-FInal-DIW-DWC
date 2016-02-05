f205d5dee359089179d5c4ef0a44a0ac21aebc36
dani izquierdo belda <dani izquierdo belda>
2016-02-05 14:16:41 +0100

    View=> filling form start

--- js/View.js
+++ js/View.js
@@ -6,6 +6,12 @@
 
 
 var View = (function () { 
+	//Page objects variables
+	var divTable;
+	var tBody;
+	var divForm;
+	var form;
+
 	//HandleBars variables
 	var HBtableSource;
 	var HBtableTemplate;
@@ -13,10 +19,11 @@ var View = (function () {
 	var HBrowSource;
 	var HBrowTemplate;
 	
+	//HandleBars templates methods
 	function tableCreator(clientsArray) {
 		HBtableSource = (typeof HBtableSource === "undefined")?$("#table-template").html():HBtableSource; 
 		HBtableTemplate = (typeof HBtableTemplate === "undefined")?Handlebars.compile(HBtableSource):HBtableTemplate; 
-		HBtableData = {clientsArray: clientsArray}
+		HBtableData = {clientsArray: clientsArray};
 		if (typeof HBrowSource === "undefined") {
 			HBrowSource = $("#row-template").html();
 			Handlebars.registerPartial("rowtemplate", HBrowSource);
@@ -29,29 +36,88 @@ var View = (function () {
 		return HBrowTemplate(cJson);
 	}
 
+	function fillForm(){
+		form = (typeof form === "undefined")?divForm.find(">:first-child"):form;
+		form[0].reset();
+		if (typeof form.inputNombre === "undefined") {
+			form.inputNombre = form.find("input[name=nombre]");
+			form.inputCiudad = form.find("input[name=ciudad]");
+			var radios = form.find("input[type=radio]");
+			form.radioSexoM = radios.eq(0);
+			form.radioSexoF = radios.eq(1);
+			form.inputTelefono = form.find("input[name=telefono]");
+			form.inputFechaNacimiento = form.find("input[name=FechaNacimiento]");
+		}
+
+		form.inputNombre.val(ClienteModel.getNombre());
+		form.inputCiudad.val(ClienteModel.getCiudad());
+		var sexo = ClienteModel.getSexo();
+		if (sexo == "M")
+			form.radioSexoM.Click();
+		else if (sexo == "M")
+			form.radioSexoF.Click();
+
+		form.inputTelefono.val(ClienteModel.getTelefono());
+		form.inputFechaNacimiento.val(ClienteModel.getFechaNacimiento());
+	}
+
+	//Show/hide methods
 	function showForm() {
-		$("#tabla").hide('slow/400/fast', function() {
-			$("#formulario").show('slow/400/fast');
+		divTable.hide('slow/200/fast', function() {
+			divForm.show('slow/200/fast');
 		});
 	}
 
 	function showTable() {
-		$("#formulario").hide('slow/400/fast', function() {
-			$("#tabla").show('slow/400/fast');
+		divForm.hide('slow/200/fast', function() {
+			divTable.show('slow/200/fast');
 		});
+	}
+
+	//Click adders
+	function addTableEventListeners() {
+		divTable.find(">#new").click(newClick);
+		tBody.find("tr .tdDelete").click(deleteClick);
+		tBody.find("tr .tdEdit").click(editClick);
+	}
+
+	function addRowEventListeners(id) {
+		tBody.find("tr#" + id + " .tdDelete").click(deleteClick);
+		tBody.find("tr#" + id + " .tdEdit").click(editClick);
+	}	
+
+	//Click functions
+	function newClick(event) {
+		View.showForm();
+
+		//.....
+	};
+
+	function deleteClick(event) {
+		alert("ID=> " + $(this).parent().attr("id"));
+		//View.showForm();
+		//.....
+	};
+
+	function editClick(event) {
+		alert("ID=> " + $(this).parent().attr("id"));
+		//View.showForm();
+		//.....
 	}
 
 	//subscribers
 	function rowInsert(_,clienteJSON) { 
-		$('#tabla>table>tbody:last-child').append(rowCreator(clienteJSON));
+		tBody.append(rowCreator(clienteJSON));
+		addRowEventListeners(clienteJSON.id);
 	}	
 
 	function rowUpdate(_,clienteJSON) { 
-		$('#' + clienteJSON.id).replaceWith(rowCreator(clienteJSON));
+		tBody.find('#' + clienteJSON.id).replaceWith(rowCreator(clienteJSON));
+		addRowEventListeners(clienteJSON.id);
 	}
 
 	function rowRemove(_,id) { 
-		$('#' + id).remove();
+		tBody.find('#' + id).remove();
 	}
 
 
@@ -65,13 +131,20 @@ var View = (function () {
 		showTable:showTable,
 
 		init: function () { 
-			//hide form and fill table
-			$('#tabla').html(tableCreator(ClientesCollection.getAll()));
+			//set divs pointers
+			divTable = $("div#tabla");
+			divForm = $("div#formulario");
+			//fill table and set tBody pointer
+			divTable.html(tableCreator(ClientesCollection.getAll()));
+			tBody =	divTable.find("table>tbody");
 
 			//subscribe to Model publish events
 			$.subscribe("insertado",rowInsert);
 			$.subscribe("actualizado",rowUpdate);
 			$.subscribe("borrado",rowRemove);
+
+			// add event listeners
+			addTableEventListeners();
 		},
 		
 
@@ -84,26 +157,6 @@ var View = (function () {
 		HBrowTemplate:HBrowTemplate
 	} 
 }()); 
-
-
-
-
-/*** add event listeners***/ 
-$("#new").click(function(event) {
-	View.showForm();
-	
-	//.....
-});
-
-$(".tdDelete").click(function(event) {
-	View.showForm();
-	//.....
-});
-
-$(".tdEdit").click(function(event) {
-	View.showForm();
-	//.....
-});
 
 /*** initialization ***/
 $.when(ClientesCollection.init()).done(function(){
