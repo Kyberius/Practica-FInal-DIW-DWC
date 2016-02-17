9eac5ea0d2a5815166b4603816cec49fd41c53ef
dani izquierdo belda <dani izquierdo belda>
2016-02-17 17:31:50 +0100

    CRUD.php more or less

--- CRUD.php
+++ CRUD.php
@@ -6,15 +6,20 @@ require_once('include_dao.php');
 $type = isset($_POST["type"])?$_POST["type"]:null;
 	
 	switch($type) {
-		case "C":echo ClienteCRUD::Create(new Cliente($_POST["nombres"],$_POST["ciudad"],$_POST["sexo"],$_POST["telefono"],$_POST["fechaNacimiento"]));break;
-		case "R":echo ClienteCRUD::Read(new Cliente($_POST["id"]));break;
-		case "U":echo ClienteCRUD::Update(new Cliente($_POST["id"],$_POST["nombres"],$_POST["ciudad"],$_POST["sexo"],$_POST["telefono"],$_POST["fechaNacimiento"]));break;
-		case "D":echo ClienteCRUD::Delete(new Cliente($_POST["id"]));break;
+		case "C":echo ClienteCRUD::Create(new ClienteCreator($_POST["nombres"],$_POST["ciudad"],$_POST["sexo"],$_POST["telefono"],$_POST["fechaNacimiento"]));break;
+		case "R":echo ClienteCRUD::Read(new ClienteCreator($_POST["id"]));break;
+		case "U":echo ClienteCRUD::Update(new ClienteCreator($_POST["id"],$_POST["nombres"],$_POST["ciudad"],$_POST["sexo"],$_POST["telefono"],$_POST["fechaNacimiento"]));break;
+		case "D":echo ClienteCRUD::Delete(new ClienteCreator($_POST["id"]));break;
 		case null:echo ClienteCRUD::All();break;
 	}
 
-class Cliente {
+class ClienteCreator {
+
+
+
 	var $clienteDAO;
+	var $Tbl;
+	var $t;
 	function __construct() {
 		$params = func_get_args();
 		$num_params = func_num_args();
@@ -24,24 +29,32 @@ class Cliente {
 		}
 	}
 
-	function __construct1($id) {
-		$this->clienteDAO = ClienteDaoObject::findId($id);
+	function __construct1($id) {//delete
+		$this->t = new Transaction();
+		$this->Tbl=DAOFactory::getClienteDAO();
+		$this->clienteDAO = new ClienteCreator();
+		$this->clienteDAO->id = $id;
+		//$this->clienteDAO = ClienteDaoObject::findId($id);
 	}
-	function __construct5($nombre,$ciudad,$sexo,$telefono,$fecha_nacimiento) {
-		$this->clienteDAO = new ClienteDaoObject();
-		$this->clienteDAO->setNombres($nombre);
-		$this->clienteDAO->setCiudad($ciudad);
-		$this->clienteDAO->setSexo($sexo);
-		$this->clienteDAO->setTelefono($telefono);
-		$this->clienteDAO->setFechaNacimiento($fecha_nacimiento);
+	function __construct5($nombre,$ciudad,$sexo,$telefono,$fecha_nacimiento) {//create
+		$this->t = new Transaction();
+		$this->Tbl=DAOFactory::getClienteDAO();
+		$this->clienteDAO = new ClienteCreator();
+		$this->clienteDAO->nombres = $nombre;
+		$this->clienteDAO->ciudad= $ciudad;
+		$this->clienteDAO->sexo = $sexo;
+		$this->clienteDAO->telefono=$telefono;
+		$this->clienteDAO->fechaNacimiento=$fecha_nacimiento;
 	}
-	function __construct6($id,$nombre,$ciudad,$sexo,$telefono,$fecha_nacimiento) {
-		$this->clienteDAO = ClienteDaoObject::findId($id);
-		$this->clienteDAO->setNombres($nombre);
-		$this->clienteDAO->setCiudad($ciudad);
-		$this->clienteDAO->setSexo($sexo);
-		$this->clienteDAO->setTelefono($telefono);
-		$this->clienteDAO->setFechaNacimiento($fecha_nacimiento);
+	function __construct6($id,$nombre,$ciudad,$sexo,$telefono,$fecha_nacimiento) {//update
+		$this->t = new Transaction();
+		$this->Tbl=DAOFactory::getClienteDAO();
+		$this->clienteDAO = $this->Tbl->load($id);
+		$this->clienteDAO->nombres = $nombre;
+		$this->clienteDAO->ciudad= $ciudad;
+		$this->clienteDAO->sexo = $sexo;
+		$this->clienteDAO->telefono=$telefono;
+		$this->clienteDAO->fechaNacimiento=$fecha_nacimiento;
 	}
 	
 	function toJSON() {
@@ -53,27 +66,34 @@ class Cliente {
 
 class ClienteCRUD{
 
-	public static function Create($cliente) {
-		$cliente->clienteDAO->save();
-		return $cliente->toJSON();
+	public static function Create($cc) {
+		$cc->Tbl->insert($cc->clienteDAO);
+		$cc->t->commit();
+		return json_encode( (array)$cc->clienteDAO);
 	}
 
-	public static function Read($cliente) {
-		return $cliente->toJSON();
+	public static function Update($cc) {
+		$cc->Tbl->update($cc->clienteDAO);
+		$cc->t->commit();
+		return json_encode( (array)$cc->clienteDAO);
 	}
 
-	public static function Update($cliente) {
-		$cliente->clienteDAO->save();
-		return $cliente->toJSON();
-	}
-
-	public static function Delete($cliente) {
-		return $cliente->clienteDAO->delete();
+	public static function Delete($cc) {
+		$cc->Tbl->delete($cc->clienteDAO->id);
+		$cc->t->commit();
+		return 1;
 	}
 	public static function All() {
-		$clienteFacotry = new ClienteDaoFactory();
-		$coll = $clienteFacotry->getCollection();
-		return json_encode($coll->toJson(),JSON_FORCE_OBJECT);
+		$t = new Transaction();
+		$arr=DAOFactory::getClienteDAO()->queryAllOrderBy('nombres');
+		$t->commit();
+		$Jarr=[];
+
+		for($i=0;$i<count($arr);$i++){
+			$c = $arr[$i];
+			$Jarr[] = json_encode( (array)$c);
+		}
+		return json_encode($Jarr,JSON_FORCE_OBJECT);
 	}
 }
  ?>
