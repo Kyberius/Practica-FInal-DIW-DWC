1db3d2ce73305679e8a9c9420e1c43c9c23d94a9
dani izquierdo belda <dani izquierdo belda>
2016-02-17 13:12:39 +0100

    min db4free relative path

--- .generate.php
+++ .generate.php
@@ -1,11 +1,11 @@
 <?php
 include("Parm/vendor/autoload.php");
-\Parm\Config::addConnection('empresa', new Doctrine\DBAL\Connection([
-    'dbname' => 'empresa',
-    'user' => 'root',
-    'password' => '',
-    'host' => 'localhost'
+\Parm\Config::addConnection('empresacrud', new Doctrine\DBAL\Connection([
+    'dbname' => 'empresacrud',
+    'user' => 'usercrud',
+    'password' => 'passwordcrud',
+    'host' => 'db4free.net'
 ], new Doctrine\DBAL\Driver\PDOMySql\Driver(), null, null));
-$generator = new Parm\Generator\Generator(Parm\Config::getConnection('empresa'),'./clases/dao/empresaDatabase',"dao\\empresaDatabase");
+$generator = new Parm\Generator\Generator(Parm\Config::getConnection('empresacrud'),'./clases/dao/empresacrudDatabase',"dao\\empresacrudDatabase");
 $generator->generate();
 ?>
